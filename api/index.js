068104fd58450d31eb7e4eef669601a2bc0e5f24
harshil35 <harshilap2004@gmail.com>
2024-01-06 11:44:25 +0300

    created user model

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
     }).catch((err) => {
         console.log(err);
-    })
+    }) 
 
 const app = express();
 
